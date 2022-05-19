bdf9ba3a732577c977ffcea8802f1a84f05fa929
hellojakesmith <jake.smith@boldcommerce.com>
2022-05-19 15:53:04 -0600

    cart updats

--- custom/custom.js
+++ custom/custom.js
@@ -1,90 +1,32 @@
-jQuery(document).ready(function ($) {
-	function addXMLRequestCallback(callback) {
-		var oldSend, i;
-		if (XMLHttpRequest.callbacks) {
-			// we've already overridden send() so just add the callback
-			XMLHttpRequest.callbacks.push(callback);
-		} else {
-			// create a callback queue
-			XMLHttpRequest.callbacks = [callback];
-			// store the native send()
-			oldSend = XMLHttpRequest.prototype.send;
-			// override the native send()
-			XMLHttpRequest.prototype.send = function () {
+jQuery(document).ready(async function ($) {
 
-				for (i = 0; i < XMLHttpRequest.callbacks.length; i++) {
-					XMLHttpRequest.callbacks[i](this);
-				}
-				// call the native send()
-				oldSend.apply(this, arguments);
-			}
-		}
-	}
+    async function getCart() {
+        const url = `${window.location.origin}/wp-json/wc/store/cart`;
+        const options = {
+            credentials: 'include',
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        };
 
-	// e.g.
-	addXMLRequestCallback(function (xhr) {
-		addListeners(xhr)
-	});
+        return await fetch(url, options).then(response => response.json());
+    }
 
-	function addListeners(xhr) {
-		xhr.addEventListener('loadend', handleEvent);
-	}
+    $(document.body).on('updated_cart_totals', async function () {
+        const cart = await getCart()
+        const api = await window.BOLD.pre.ready()
+        const boldCart = await api.getCart()
 
-	async function handleEvent(e) {
-		if (e && e.currentTarget && e.currentTarget.responseURL && e.currentTarget.responseURL.includes('get_refreshed_fragments')) {
-			await getCart().then(data => {
-				if (data && data.items) {
-					data.items.map(item => {
-						window.BOLD.pre.events.emit('cart_updated_qty', { "qty": item.quantity, "id": item.key });
-					})
-				}
-			})
-		}
+        boldCart?.items.forEach(item => {
+            let cartItemToAdd = cart?.items.find(cartItem => cartItem.key === item.id)
+            let quantity = cartItemToAdd && cartItemToAdd.quantity ? cartItemToAdd.quantity : 0
+            window.BOLD.pre.events.emit('cart_updated_qty', { "qty": quantity, "id": item.id });
+        })
 
-		if (e && e.currentTarget && e.currentTarget.responseURL && e.currentTarget.responseURL.includes(`removed_item`)) {
-			await getCart().then(data => {
-				if(data && data.items_count === 0){
-					window.BOLD && window.BOLD.common && window.BOLD.common.eventEmitter.emit("BOLD_NEW_CART", { lineItems: {"items":[],"products":[],"cart_total":0} });
-					
-					$( document ).trigger( 'wc_update_cart' );
-				}
-				if (data && data.items && data.items_count >= 1) {
-					let platformData = JSON.parse($('#bold-platform-data').html());
-					let boldCart = platformData['cart']
 
-					let boldCartItems = Object.keys(boldCart['items']).filter(i => data.items.find(x => x.key === i))
-					let mergedItems = boldCartItems.map(x => {
-						if (x === boldCart['items'][x].key) {
-							let wooItem = data.items.find(item => item.key === boldCart['items'][x].key)
-
-							boldCart['items'][x].quantity = wooItem.quantity
-							return { [x]: boldCart['items'][x] }
-						}
-					})
-
-					let cartItems = Object.assign.apply(boldCart['items'], mergedItems)
-					let cartProducts = boldCart.products.filter(i => data.items.find(x => x.id === i.id))
-
-					let cartTotal = (data.totals.total_items / 100).toFixed(2)
-
-					window.BOLD && window.BOLD.common && window.BOLD.common.eventEmitter.emit("BOLD_NEW_CART", { lineItems: { "items": cartItems, "products": cartProducts, "cart_total": cartTotal } });
-					
-					$( document ).trigger( 'wc_update_cart' );
-				}
-			})
-		}
-	}
-
-	async function getCart() {
-		const url = `${window.location.origin}/wp-json/wc/store/cart`;
-		const options = {
-			credentials: 'include',
-			method: "GET",
-			headers: {
-				'Content-Type': 'application/json'
-			},
-		};
-
-		return await fetch(url, options).then(response => response.json());
-	}
-});
+    });
+    $(document.body).on('wc_cart_emptied', async function () {
+        window.BOLD && window.BOLD.common && window.BOLD.common.eventEmitter.emit("BOLD_NEW_CART", { lineItems: { "items": [], "products": [], "cart_total": 0 } });
+    })
+})
